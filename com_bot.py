a229a3d74057e2ac6972c0b97e781e72de00e5f7
Chanel Matlock <chanel.matlock@clover.com>
2018-05-07 14:29:48 -0700

    Fully functional while running locally using ngrok

--- com_bot.py
+++ com_bot.py
@@ -1,91 +1,40 @@
-# Following https://www.fullstackpython.com/blog/build-first-slack-bot-python.html
+# pylint: disable=missing-docstring
 
-import requests
-import os
-import time
-import re
-from slackclient import SlackClient
+# import sys
+# sys.path.append("lib")
+
 import json
-import schedule
-import config
 from datetime import datetime
 from datetime import timedelta
 
-ANSWERHUB_USER = config.ANSWERHUB_USER
-ANSWERHUB_PASS = config.ANSWERHUB_PASS
-url = "https://community.clover.com/services/v2/question.json?unanswered=true&pageSize=100&sort=newest&includeOnly=id,slug,title,lastActiveDate"
-base_url =  config.BASE_URL
+import requests
+import webapp2 # webob, paste
 
-slack_client = SlackClient(config.SLACK_BOT_TOKEN)
-ccbot_id = None
+import config
 
-# constants
-RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
-EXAMPLE_COMMAND = "do"
-MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+AUTH = {'user': config.ANSWERHUB_USER, 'password': config.ANSWERHUB_PASS}
+URL = (
+    "https://community.clover.com/services/v2/question.json?unanswered=true&pageSize=100"
+    "&sort=newest&includeOnly=id,slug,title,lastActiveDate"
+)
+BASE_URL = config.BASE_URL
 
-
-def parse_bot_commands(slack_events):
-    """
-    Parses a list of events coming from the Slack RTM API to find bot commands.
-    If a bot command is found, this function returns a tuple of command and channel.
-    If its not found, then this function returns None, None.
-    """
-    for event in slack_events:
-        if event["type"] == "message" and not "subtype" in event:
-            user_id, message = parse_direct_mention(event["text"])
-            if user_id == starterbot_id:
-                return message, event["channel"]
-    return None, None
-
-
-def parse_direct_mention(message_text):
-    """
-    Finds a direct mention (a mention that is at the beginning) in message text
-    and returns the user ID which was mentioned. If there is no direct mention, returns None
-    """
-    matches = re.search(MENTION_REGEX, message_text)
-    # the first group contains the username, the second group contains the remaining message
-    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
-
-
-def handle_command(command, channel):
-    """
-    Executes bot command if the command is known
-    """
-    # Default response is help text for the user
-    default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
-
-    # Finds and executes the given command, filling in response
-    response = None
-    # This is where you start to implement more commands!
-    if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
-
-    if command.startswith("newest"):
-        response = daily_digest()
-
-    # Sends the response back to the channel
-    slack_client.api_call(
-        "chat.postMessage",
-        channel=channel,
-        text=response or default_response
-    )
-
-
-def check_for_command():
-    command, channel = parse_bot_commands(slack_client.rtm_read())
-    if command:
-        handle_command(command, channel)
-
+SLACK_WEB_HOOK = config.SLACK_WEB_HOOK
+CCBOT_ID = None
 
 def daily_digest():
-    web_response = json.loads(requests.get(url, auth=(ANSWERHUB_USER, ANSWERHUB_PASS)).text)
-    response = "Daily Question List: "
+    # web_response = json.loads(requests.get(url, auth=(auth["user"], auth["password"])).text)
+    comm_response = requests.get(URL, auth=(AUTH["user"], AUTH["password"])).text
+    web_response = json.loads(comm_response)
+
+    response = ""
     top_questions = top_ten_questions(web_response["list"])
     for question in top_questions:
-        response += ("\n   - *[" + time_label(datetime.fromtimestamp(question["lastActiveDate"]/1e3)) + "]* <" + base_url + str(question["id"]) + "/" + question["slug"] + ".html|" + question["title"] + ">")
-
+        response += (
+            "\n- *[" + time_label(timestamp_to_date(question["lastActiveDate"])) + "]* <" +
+            BASE_URL + str(question["id"]) + "/" + question["slug"] + ".html|" +
+            question["title"] + ">"
+        )
     return response
 
 def top_ten_questions(question_list):
@@ -93,36 +42,59 @@ def top_ten_questions(question_list):
 
     chosen_ones = []
     for question in sorted_web_response:
-        if all([older_than_two_days(question), younger_than_one_month(question)]):
+        if all([
+                older_than_two_days(question["lastActiveDate"]),
+                younger_than_one_month(question["lastActiveDate"])
+        ]):
             chosen_ones.append(question)
-            if len(chosen_ones) == 10:
+            if len(chosen_ones) >= 10:
                 break
     return chosen_ones
 
 def time_label(question_time):
     age = datetime.now() - question_time
-    return (str(age.days) + " days ago")
+    return str(age.days) + " days ago"
 
 ### Requirements for top questions
 
-def older_than_two_days(question):
-    return (datetime.fromtimestamp(question["lastActiveDate"]/1e3)) <= (datetime.now() - timedelta(days=2))
+def older_than_two_days(timestamp):
+    return timestamp_to_date(timestamp) <= (datetime.now() - timedelta(days=2))
 
-def younger_than_one_month(question):
-    return (datetime.fromtimestamp(question["lastActiveDate"]/1e3)) >= (datetime.now() - timedelta(days=30))
+def younger_than_one_month(timestamp):
+    return timestamp_to_date(timestamp) >= (datetime.now() - timedelta(days=30))
+
+def timestamp_to_date(milliseconds):
+    return datetime.fromtimestamp(milliseconds/1e3)
+
+class DailyDigest(webapp2.RequestHandler):
+    def post(self):
+        text = "Here you go!"
+        fallback = "Required plain-text summary of the attachment."
+        color = "#2eb886"
+        pretext = "Your Daily Digest"
+        attachments = [{
+            "fallback": fallback,
+            "color": color,
+            "pretext": pretext,
+            "text": daily_digest()
+        }]
+        slack_response = {"text": text, "attachments": attachments}
+        response_object = webapp2.Response(json.dumps(slack_response))
+        response_object.content_type = "application/json"
+
+        return response_object
 
 
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+
+APP = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/daily_digest', DailyDigest),
+], debug=True)
 
 if __name__ == "__main__":
-    if slack_client.rtm_connect(with_team_state=False):
-        print("Starter Bot connected and running!")
-        # Read bot's user ID by calling Web API method `auth.test`
-        starterbot_id = slack_client.api_call("auth.test")["user_id"]
-
-        # Set the schedules
-        schedule.every(1).second.do(check_for_command)
-        schedule.every().day.at("8:30").do(daily_digest)
-        while True:
-            schedule.run_pending()
-    else:
-        print("Connection failed. Exception traceback printed above.")
+    from paste import httpserver
+    httpserver.serve(APP, host='127.0.0.1', port='8080')

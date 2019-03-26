4f27cba162f736fd0ae0ab984b56839c6b0905d7
Chanel Matlock <chanel.matlock@clover.com>
2019-03-26 16:29:30 -0700

    Threading actually works. Before 50 questions at 8 seconds. Now 100 questions at 7 seconds. Praise be the Silicon Deity.

--- common.py
+++ common.py
@@ -2,6 +2,7 @@
 
 from datetime import timedelta
 from datetime import datetime
+import threading
 import json
 import requests
 import config
@@ -27,6 +28,7 @@ PAGE_SIZE_100 = "pageSize=100"
 PAGE_SIZE_50 = "pageSize=50"
 PAGE_SIZE_30 = "pageSize=30"
 PAGE_SIZE_10 = "pageSize=10"
+PAGE_SIZE_1 = "pageSize=1"
 
 SORT_HOTTEST = "sort=hottest"
 SORT_NEWEST = "sort=newest"
@@ -59,18 +61,38 @@ PAGE_HEADER = "<head>" + CSS_LINK + JS_LINK + "</head>"
 WEB_PAGE_START = PAGE_HEADER + "<body>"
 WEB_PAGE_END = "</body>" + END_HTML
 
+CLOVER_OWNED_QUESTIONS = []
+clover_lock = threading.Lock()
+
+USER_OWNED_QUESTIONS = []
+user_lock = threading.Lock()
+
 class QuestionCard:
     def __init__(self, question):
         self.question_json = question
+        self.id = question['id']
+        self.slug = question['slug']
         self.title = question['title']
+        self.lastActiveDate = question['lastActiveDate']
+        self.lastActiveUserId = question['lastActiveUserId']
+        self.childrenIds = question['childrenIds']
+        self.commentIds = question['commentIds']
+        self.answers = question['answers']
+        self.author = question['author']
+        self.marked = question['marked']
+
         self.browser_url = create_question_url(question)
+        self.answers_url_api = self.create_question_answers_api()
+        self.comments_url_api = self.create_question_comments_api()
+        self.status = self.question_status()
         self.answer_count = question['answerCount']
         self.last_active = timestamp_to_date(question["lastActiveDate"])
 
-        self.delete_url = create_question_delete_api(question)
+        self.delete_url = self.create_question_delete_api()
         self.answers = []
+        self.html_card = self.html_bootstrap_card()
 
-    def display_question(self):
+    def console_question(self):
         print '{:^80}'.format("Current Question")
 
         print '{:<80}'.format("Title       : " + self.title)
@@ -80,7 +102,7 @@ class QuestionCard:
 
         print '{:~^80}'.format("")
 
-    def display_answers(self):
+    def console_answers(self):
         if self.answer_count == 0:
             print '{:=^80}'.format("No Answers Posted")
             return
@@ -94,8 +116,110 @@ class QuestionCard:
             print '{:<80}'.format("Body: " + answer['body'])
             print '{:-^80}'.format("")
 
+    def html_bootstrap_card(self):
+        response = ""
+        question_card_start = "<div class='card question border-" + self.status + "'>"
+        question_card_end = "</div>"
 
+        response += question_card_start
+        response += self.question_header()
+        response += self.question_body()
+        response += self.question_footer()
+        response += question_card_end
 
+        return response
+
+    def question_header(self):
+        response = ""
+
+        question_title_start = "<div class='card-header font-weight-bold text-truncate bg-" + self.status + "'>"
+        question_title_end = "</div>"
+
+        response += question_title_start
+        response += self.title
+        response += question_title_end
+
+        return response
+
+    def question_body(self):
+        response = "<div class='card-body bg-transparent'>"
+
+        response += self.all_users_comments(self.comments_url_api)
+        response += self.all_users_answers(self.answers_url_api)
+
+        return response + "</div>"
+
+    def question_footer(self):
+        response = ""
+
+        question_footer_start = "<div class='card-footer font-italic border-" + self.status + "'>"
+        question_footer_end = "</div>"
+
+        response += question_footer_start
+        response += "<div class='float-left'>" + create_time_label(self.lastActiveDate) + "</div>"
+        response += "<a href='" + self.browser_url + "' class='btn btn-sm btn-dark float-right' target='_blank'> View </a>"
+        response += question_footer_end
+
+        return response
+
+    def question_status(self):
+        # returns Bootstrap keyword for color/formatting
+
+        if self.marked:
+            return "success"
+
+        # Not an accurate representation; find a better indicator
+        if (len(self.childrenIds)) == 0:
+            return "danger"
+
+        return "warning"
+
+    def all_users_comments(self, question):
+        user_list = "<dt>Commenting Users:</dt>"
+        unique_names = []
+
+        comments = get_results(question)["list"]
+
+        for comment in comments:
+            name = comment["author"]["username"]
+            if name not in unique_names:
+                unique_names.append(name)
+                user_list += (
+                    "<dd>- " + str(name) + "</dd>"
+                )
+
+        if not unique_names:
+            user_list += "<dd>No one has commented...</dd>"
+
+        return user_list
+
+    def all_users_answers(self, question):
+        user_list = "<dt>Answering Users: </dt>"
+        unique_names = []
+
+        answers = get_results(question)["list"]
+
+        for answer in answers:
+            name = answer["author"]["username"]
+            if name not in unique_names:
+                unique_names.append(name)
+                user_list += (
+                    "<dd>- " + str(name) + "</dd>"
+                )
+
+        if not unique_names:
+            user_list += "<dd>No one has answered...</dd>"
+
+        return user_list
+
+    def create_question_delete_api(self):
+        return BASE_URL + ANSWERHUB_API + "node/" + str(self.id) + "/delete" + JSON
+
+    def create_question_comments_api(self):
+        return BASE_URL + ANSWERHUB_QUESTION_API + "/" + str(self.id) + "/comment" + JSON
+
+    def create_question_answers_api(self):
+        return BASE_URL + ANSWERHUB_QUESTION_API + "/" + str(self.id) + "/answer" + JSON
 
 def get_results(url):
     comm_response = requests.get(url, auth=(AUTH["user"], AUTH["password"])).text
@@ -104,6 +228,14 @@ def get_results(url):
 def put_results(url):
     comm_response = requests.put(url, auth=(AUTH["user"], AUTH["password"])).text
     return comm_response
+
+def get_clover_ids():
+    clover_users = get_results(CLOVER_USERS_URL)["list"]
+    ids = []
+    for user in clover_users:
+        ids.append(user.get("id"))
+
+    return ids
 
 def time_label(question_time):
     age = datetime.now() - question_time
@@ -124,14 +256,15 @@ def create_time_label(question_timestamp):
 def create_question_url(question):
     return QUESTION_URL + "/" + str(question["id"]) + "/" + question["slug"] + ".html"
 
-def create_question_delete_api(question):
-    return BASE_URL + ANSWERHUB_API + "node/" + str(question["id"]) + "/delete" + JSON
-
-def create_question_comments_api(question):
-    return BASE_URL + ANSWERHUB_QUESTION_API + "/" + str(question["id"]) + "/comment" + JSON
-
-def create_question_answers_api(question):
-    return BASE_URL + ANSWERHUB_QUESTION_API + "/" + str(question["id"]) + "/answer" + JSON
-
 def get_page_of_questions(num, base_url):
     return get_results(base_url + "&page=" + num)["list"]
+
+def add_to_user_questions(question):
+    user_lock.acquire()
+    USER_OWNED_QUESTIONS.append(question)
+    user_lock.release()
+
+def add_to_clover_questions(question):
+    clover_lock.acquire()
+    CLOVER_OWNED_QUESTIONS.append(question)
+    clover_lock.release()
--- overview.py
+++ overview.py
@@ -1,26 +1,17 @@
 # pylint: disable=missing-docstring
 
 import webapp2
-import threading
-import common
 
-array_lock = threading.Lock()
-CLOVER_IDs = []
+import common
+import communityCrawler
 
 def overview():
-    CLOVER_IDs = CLOVER_IDs or get_clover_ids
-
     relevant_url = (
         common.QUESTION_JSON_URL + "?" + common.APP_MARKET_SPACE + "&"+ common.ONLY_UNANSWERED +
-        "&" + common.PAGE_SIZE_10 + "&" + common.INCLUDED_VALUES
+        "&" + common.PAGE_SIZE_1 + "&" + common.INCLUDED_VALUES
     )
-
-    # question_list = common.get_results(
-    #     common.QUESTION_JSON_URL + "?" + common.APP_MARKET_SPACE + "&"+ common.ONLY_UNANSWERED +
-    #     "&" + common.PAGE_SIZE_100 + "&" + common.INCLUDED_VALUES
-    # )["list"]
     
-    question_list = release_the_sniffers(relevant_url)
+    create_start_crawlers(relevant_url)
 
     overview_start = "<div class='container'><h1>App Market Community Overview</h1>"
     overview_end = "</div>"
@@ -58,120 +49,15 @@ def overview():
     # Meant to be on a webpage, so building HTML
     response = common.WEB_PAGE_START + overview_start + legend
 
-    clover_questions, user_questions = separate_by_last_active_user(question_list, clover_ids)
+    response += "<h2>Last Response From Users</h2>" + "<div class='card-deck'>"
+    response += prepare_cards(sorted(common.USER_OWNED_QUESTIONS, key=lambda x_y: x_y.lastActiveDate)) + "</div>"
 
-    response += "<h2>Last Response From Users</h2>"
-    response += populate_cards(sorted(user_questions, key=lambda x_y: x_y["lastActiveDate"]))
-
-    response += "<h2>Last Response From Clover</h2>"
-    response += populate_cards(sorted(clover_questions, key=lambda x_y: x_y["lastActiveDate"]))
+    response += "<h2>Last Response From Clover</h2>" + "<div class='card-deck'>"
+    response += prepare_cards(sorted(common.CLOVER_OWNED_QUESTIONS, key=lambda x_y: x_y.lastActiveDate)) + "</div>"
 
     response += overview_end + common.WEB_PAGE_END
 
     return response
-
-def get_clover_ids():
-    clover_users = common.get_results(common.CLOVER_USERS_URL)["list"]
-    ids = []
-
-    for user in clover_users:
-        ids.append(user.get("id"))
-    return ids
-
-def question_header(question, status):
-    response = ""
-
-    question_title_start = "<div class='card-header font-weight-bold text-truncate bg-" + status + "'>"
-    question_title_end = "</div>"
-
-    response += question_title_start
-    response += question["title"]
-    response += question_title_end
-
-    return response
-
-def question_body(question):
-    response = "<div class='card-body bg-transparent'>"
-
-    response += all_users_comments(common.create_question_comments_api(question))
-    response += all_users_answers(common.create_question_answers_api(question))
-
-    return response + "</div>"
-
-def question_footer(question, status):
-    response = ""
-
-    question_footer_start = "<div class='card-footer font-italic border-" + status + "'>"
-    question_footer_end = "</div>"
-
-    response += question_footer_start
-    response += "<div class='float-left'>" + common.create_time_label(question["lastActiveDate"]) + "</div>"
-    response += "<a href='" + common.create_question_url(question) + "' class='btn btn-sm btn-dark float-right' target='_blank'> View </a>"
-    response += question_footer_end
-
-    return response
-
-def question_status(question):
-    # returns Bootstrap keyword for color/formatting
-
-    if question["marked"]:
-        return "success"
-
-    # Not an accurate representation; find a better indicator
-    if (len(question["childrenIds"])) == 0:
-        return "danger"
-
-    return "warning"
-
-def all_users_comments(question):
-    user_list = "<dt>Commenting Users:</dt>"
-    unique_names = []
-
-    comments = common.get_results(question)["list"]
-
-    for comment in comments:
-        name = comment["author"]["username"]
-        if name not in unique_names:
-            unique_names.append(name)
-            user_list += (
-                "<dd>- " + str(name) + "</dd>"
-            )
-
-    if not unique_names:
-        user_list += "<dd>No one has commented...</dd>"
-
-    return user_list
-
-def all_users_answers(question):
-    user_list = "<dt>Answering Users: </dt>"
-    unique_names = []
-
-    answers = common.get_results(question)["list"]
-
-    for answer in answers:
-        name = answer["author"]["username"]
-        if name not in unique_names:
-            unique_names.append(name)
-            user_list += (
-                "<dd>- " + str(name) + "</dd>"
-            )
-
-    if not unique_names:
-        user_list += "<dd>No one has answered...</dd>"
-
-    return user_list
-
-def separate_by_last_active_user(question_list, id_list):
-    clovers_questions = []
-    users_questions = []
-
-    for question in question_list:
-        if question["lastActiveUserId"] in id_list:
-            clovers_questions.append(question)
-        else:
-            users_questions.append(question)
-
-    return clovers_questions, users_questions
 
 def populate_card_deck():
     # start the deck of cards section
@@ -180,66 +66,23 @@ def populate_card_deck():
     return response + "</div>"
 
 def prepare_cards(questions):
-    
-    
+    response = ""
     for question in questions:
-        status = question_status(question)
+        response += question.html_card
 
-        question_card_start = "<div class='card question border-" + status + "'>"
-        question_card_end = "</div>"
+    return response
 
-        response += question_card_start
-        response += question_header(question, status)
-        response += question_body(question)
-        response += question_footer(question, status)
-        response += question_card_end
+def create_start_crawlers(relevant_url):
+    crawler_list = []
 
-def create_sniffer(num, base_url):
-    t = threading.Thread(name=("sniffer"+num), target=start_sniffers, args=(num, base_url))
-    return t
+    for i in range(10):
+        crawler_list.append(communityCrawler.communityCrawler(relevant_url, i))
 
-def start_sniffers(num, base_url):
-    print ">>>sniffer " + num + " started "
-    questions = []
+    for crawler in crawler_list:
+        crawler.start()
 
-    if int(num) > 1:
-        questions = common.get_page_of_questions(num, base_url)
-
-    clover_questions, user_questions = separate_by_last_active_user(questions, CLOVER_IDs)
-    
-
-    for question in questions:
-        with array_lock:
-            RETURNED_QUESTIONS.append(question)
-
-    print "<<sniffer " + num + " completed"
-
-def release_the_sniffers(relevant_url):
-    sniffer_list = []
-
-    web_response = common.get_results(relevant_url)
-
-    page_count = web_response["pageCount"]
-
-    if int(page_count) > 10:
-        page_count = 10
-
-    RETURNED_QUESTIONS.extend(web_response["list"])
-
-    for i in range(page_count):
-        sniffer_list.append(create_sniffer(str(i+1), relevant_url))
-
-    print 'So, {0} pages found, so {1} sniffer(s) popped into existence.'.format(page_count, len(sniffer_list))
-    print 'No threads started yet.'
-
-    for sniffer in sniffer_list:
-        sniffer.start()
-
-    for sniffer in sniffer_list:
-        sniffer.join()
-
-    print " final question length: " + str(len(RETURNED_QUESTIONS))
-    return RETURNED_QUESTIONS
+    for crawler in crawler_list:
+        crawler.join()
 
 class Overview(webapp2.RequestHandler):
     def get(self):
